8ff30ea91ba6dc4340bd38547332614e7a2b7ed3
itsmearion <bilqiss0893@gmail.com>
2025-05-02 06:13:48 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ API_ID = 29386534
 API_HASH = "8f35dec4c4de801ec648cf4ca1cf04e9"
 BOT_TOKEN = "7766823813:AAHOtLoVip5vxtKbwly1kt3JTX7FwN1Ko_M"
 ADMIN_IDS = [1407585501, 6467919046]  # ganti dengan ID admin bot kamu
-CHANNEL_ID = -1001625465798  # ganti dengan ID channel admin
+CHANNEL_ID = -1002512972708  # ganti dengan ID channel admin
